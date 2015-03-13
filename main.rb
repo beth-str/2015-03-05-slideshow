994fe3bb60e166c6f2bc8ca53fd4e753fd779b21
Beth Stranz <bethstranz@hotmail.com>
2015-03-13 09:43:24 -0500

    Added configure :production

--- main.rb
+++ main.rb
@@ -8,6 +8,19 @@ require 'json'
 
 require 'sinatra/activerecord'
 
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'])
+   ActiveRecord::Base.establish_connection(
+     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+     :host => db.host,
+     :username => db.user,
+     :password => db.password,
+     :database => db.path[1..-1],
+     :encoding => 'utf8'
+ )
+end
+
+
 configure :development do
   set :database, {adapter: "sqlite3", database: "database/slideshow.db"}
 end
