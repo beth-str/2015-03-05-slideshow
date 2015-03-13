369cbca756cd2cf85bd8cd49dd4b86c9e2865934
Beth Stranz <bethstranz@hotmail.com>
2015-03-13 10:30:23 -0500

    Added db setup for ActiveRecord

--- database/db_setup.rb
+++ database/db_setup.rb
@@ -1,11 +1,36 @@
 require 'sqlite3'
 
-DATABASE = SQLite3::Database.new("database/slideshow.db")
+# copy once for each table: (team_id is foreign key)
+
+unless ActiveRecord::Base.connection.table_exists?(:slides)
+   ActiveRecord::Base.connection.create_table :slides do |t|
+     t.text :title
+     t.text :body
+   end
+end
+
+unless ActiveRecord::Base.connection.table_exists?(:users)
+   ActiveRecord::Base.connection.create_table :users do |t|
+     t.text :username
+     t.text :password
+   end
+end
 
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS slides
-                (id INTEGER PRIMARY KEY,
-                title TEXT NOT NULL,
-                body TEXT NOT NULL)")
-
-DATABASE.results_as_hash = true
+# DATABASE = SQLite3::Database.new("database/slideshow.db")
+#
+#
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS slides
+#                 (id INTEGER PRIMARY KEY,
+#                 title TEXT NOT NULL,
+#                 body TEXT NOT NULL)")
+#
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS users
+#                 (id INTEGER PRIMARY KEY,
+#                 username TEXT NOT NULL,
+#                 password TEXT NOT NULL)")
+#
+#
+#
+#
+# DATABASE.results_as_hash = true
